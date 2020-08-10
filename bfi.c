74b49ff6fe6278a3377ce13da97efa45993faac3
Sadeli <txiinow@gmail.com>
2020-08-10 10:30:32 -1000

    Make all operators functional

--- bfi.c
+++ bfi.c
@@ -1,12 +1,18 @@
+// @author Thad Shinno
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
+char*file2str(char*);
+int isbf(char);
 // <>+-[],.
-int main(void) {
-    char *code = ">+++++++++[<++++++++>-]<.>+++++++[<++++>-]<+.+++++++..+++.[-]>++++++++[<++++>-] <.>+++++++++++[<++++++++>-]<-.--------.+++.------.--------.[-]>++++++++[<++++>- ]<+.[-]++++++++++.";
+
+int main(int argc, char*argv[]) {
+    // get code
+    char *code = file2str(argv[1]);
+    // printf("%s", code);
+    
     // init tape
-    int size = 0; // size of tape is 0
+    int tapeSize = 1;
     char *tape = (char*)malloc(sizeof(char)); // allocate memory
     memset(tape, 0, 1); // set it to 0
 
@@ -14,29 +20,99 @@ int main(void) {
     char *ptr = &tape[0];
     int cur = 0; // current block is index 0
 
-    for (char c = 0; code[c] != '\0'; c++) {
-        switch (code[c]) {
+    // input char
+    char in;
+    // stack to store while [ indexes
+    int stackSize = 0;
+    int* stack = (int*)malloc(sizeof(int));
+
+    // execute code
+    for (int i = 0; code[i] != '\0'; i++) {
+        switch (code[i]) {
             case '>':
                 ptr++; // move pointer to the right one
                 cur++; // increase cur
                 // increase size if need to
-                if (cur >= size) {
-                    size++;
-                    tape = realloc(tape, size*sizeof(char));
-                    memset(tape+cur, 0, 1); // set the new one to 0
+                if (cur >= tapeSize) {
+                    tapeSize++;
+                    tape = realloc(tape, tapeSize*sizeof(char));
+                    memset(tape+cur, 0, 1); // set the new block to 0
                 }
                 break;
             case '<':
-                ptr--;
-                cur--;
+                ptr--; // move pointer to the left one
+                cur--; // decrease cur
                 break;
             case '+':
-                (*ptr)++;
+                (*ptr)++; // increase current block by one
                 break;
             case '-':
-                (*ptr)--;
+                (*ptr)--; // decrease current block by one
+                break;
+            case '[':
+                // add the index of the operater AFTER [ to the stack
+                stackSize++;
+                stack = realloc(stack, stackSize*sizeof(int));
+                // memset(stack+stackSize-1, i+1, 1);
+                stack[stackSize-1] = i;
+                break;
+            case ']':
+                // if the current block is zero, pop the stack and move on
+                if (*ptr == 0) {
+                    stackSize--;
+                    stack = realloc(stack, stackSize*(sizeof(int)));
+                    break;
+                }
+                // otherwise, go back to the operater AFTER [
+                else {
+                    i = stack[stackSize-1];
+                    break;
+                }
+                break;
+            case '.':
+                printf("%c", *ptr); // print current block
+                break;
+            case ',':
+                // get a single char and put it into the current block
+                printf("Enter a character: ");
+                in = getchar();
+                *ptr = in;
                 break;
         }
     }
     free(tape);
+}
+
+char *file2str(char* filename) {
+    FILE *fp;
+    fp = fopen(filename, "r");
+    if (!fp) {
+        printf("File not found\n");
+        exit(1);
+    }
+    // init out
+    char c;
+    int size = 0;
+    char *out = (char*)malloc(sizeof(char));
+    do {
+        c = fgetc(fp);
+        if (isbf(c)) {
+            size++;
+            out = realloc(out, size*(sizeof(char)));
+            memset(out+size-1, c, 1);
+        }
+    } while (c != EOF);
+    fclose(fp);
+    return out;
+}
+
+int isbf(char c) {
+    // + , - .
+    if ('+' <= c && c <= '.') {
+        return 1;
+    }
+    // < > [ ]
+    else {
+        return (c == '<' || c == '>' || c == '[' || c==']');
+    }
 }
